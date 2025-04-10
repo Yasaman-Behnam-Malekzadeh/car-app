f5ae67429df52b92aa5654c61cc96bab3d1284dc
Yasaman-Behnam-Malekzadeh <malekzadeh.yasi@yahoo.com>
2025-04-10 16:58:26 +0200

    Change some styles

--- app/components/SearchBox.tsx
+++ app/components/SearchBox.tsx
@@ -10,8 +10,8 @@ export default function SearchBox({onSearch}:SearchBoxProps) {
   
   return (
     <input
-      className="w-2/3 mb-5 ml-4 border-2 p-3 rounded-sm border-gray-200"
-      placeholder="Search about your car..."
+      className="w-1/2 mb-5 ml-4 border-2 p-3 rounded-sm border-gray-200"
+      placeholder="Search ..."
       onChange={(e) => onSearch(e.target.value)}
     />
   );
