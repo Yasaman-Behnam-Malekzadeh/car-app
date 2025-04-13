cc665691518bf9ae21021e660cf1974f68627c18
Yasaman-Behnam-Malekzadeh <malekzadeh.yasi@yahoo.com>
2025-04-13 20:25:36 +0200

    Add pagination for all cars

--- app/components/CarList.tsx
+++ app/components/CarList.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import CarCard from "./CarCard";
 import SearchBox from "./SearchBox";
+import Pagination from "react-js-pagination";
 
 interface car {
   id: string;
@@ -19,7 +20,18 @@ interface car {
 
 export default function CarList() {
   const [data, setData] = useState<car[]>([]);
-  const [searchData,setSearchData]= useState<string>('')
+  const [searchData, setSearchData] = useState<string>("");
+
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const itemPerPage = 8;
+  const indexOfLastItem = currentPage * itemPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemPerPage;
+
+  const filteredCar = data.filter((car) => {
+    return car.make.toLowerCase().includes(searchData.toLowerCase());
+  });
+
+  const currentItems = filteredCar.slice(indexOfFirstItem, indexOfLastItem);
 
   useEffect(() => {
     async function fetchData() {
@@ -39,19 +51,29 @@ export default function CarList() {
     fetchData();
   }, []);
 
-  const filteredCar= data.filter((car)=>{
-    return car.make.toLowerCase().includes(searchData.toLowerCase())
-  })
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
 
-
-  
   return (
     <div className="container m-auto">
-      <SearchBox onSearch={setSearchData}/>
-      <div className="flex flex-wrap justify-between">
-        {filteredCar.map((item) => (
+      <SearchBox onSearch={setSearchData} />
+      <div className="flex flex-wrap ml-[-22px]">
+        {currentItems.map((item) => (
           <CarCard key={item.id} car={item} />
         ))}
+      </div>
+      <div className="flex justify-center my-8 ">
+        <Pagination
+          activePage={currentPage}
+          onChange={handlePageChange}
+          itemsCountPerPage={itemPerPage}
+          totalItemsCount={filteredCar.length}
+          pageRangeDisplayed={8}
+          itemClass="mx-1"
+          linkClass="px-3 py-1 rounded-lg bg-gray-100 hover:bg-blue-500 hover:text-white text-sm text-gray-800 transition-colors"
+          innerClass="flex gap-2"
+        />
       </div>
     </div>
   );
