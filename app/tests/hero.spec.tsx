6505addc7d0e59a3a6924bba3db26fba4dd7aa26
Yasaman-Behnam-Malekzadeh <malekzadeh.yasi@yahoo.com>
2025-04-15 13:22:39 +0200

    Update playwright test for Hero page

--- app/tests/hero.spec.tsx
+++ app/tests/hero.spec.tsx
@@ -5,10 +5,15 @@ test("Show the hero page with image and texts", async ({ page }) => {
 
   const image = await page.getByAltText("hero");
   await expect(image).toBeVisible;
-  const BGImage= await page.getByAltText("hero-background")
-  await expect(BGImage).toBeVisible
+  const BGImage = await page.getByAltText("hero-background");
+  await expect(BGImage).toBeVisible;
 
-
-  const text=await page.getByText("Find and Show details of a car—quick and super easy!")
-  await expect(text).toBeVisible
+  const text = await page.getByText(
+    "Find and Show details of a car—quick and super easy!"
+  );
+  await expect(text).toBeVisible;
+  const secondText = await page.getByText(
+    "This is a case study for a Front end developer!"
+  );
+  await expect(secondText).toBeVisible;
 });
